8b3dc17c8205d66862829c4bbef56a8c07df92be
ibrahimmdawoodd100 <74507430+ibrahimmdawoodd100@users.noreply.github.com>
2026-08-03 15:48:04 +0300

    Add files via upload

---
+++ employee/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'basma-employee-v2';
+const SHELL_FILES = ['./index.html', './icon-employee-192.png', './icon-employee-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
